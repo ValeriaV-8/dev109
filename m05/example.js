fa32d2f0a88553c812ced3550b4a1457ee6b496b
ValeriaV-8 <james010698@gmail.com>
2025-02-06 20:42:06 -0800

    initial push

--- m05/example.js
+++ m05/example.js
@@ -1,1 +1,29 @@
+var string = window.prompt("Please enter a number between 0 - 10");
+var num = string*1
+if (isNumber(num) === 'True')
+  return num;
+if (isNumber(num) === 'False')
+  num = 0;
 
+var table = num;            // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';   // Message
+
+if (operator === 'multiplication') {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+} /* else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+} */
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
