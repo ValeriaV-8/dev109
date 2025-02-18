bf0245eb2d4a8153171c6fd965e548f62606bcb1
ValeriaV-8 <james010698@gmail.com>
2025-02-18 14:18:08 -0800

    Update example.js
    Removed unused code

--- m05/example.js
+++ m05/example.js
@@ -14,14 +14,7 @@ if (operator === 'multiplication') {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-} /* else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
-} */
-
+}
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
